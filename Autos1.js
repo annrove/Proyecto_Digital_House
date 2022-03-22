890507ba7d4aff7a8d6912feed55645195e1da5b
annrove <annrove@gmail.com>
2022-03-22 13:42:21 -0500

    Nuevos Cambios a los archivos del Proyecto 1:42_22/03/22

--- Autos1.js
+++ Autos1.js
@@ -1,12 +1,17 @@
 const autos = require('./autos')
+//Esto fue explicado por Fabio para que visualizara que la función podía funcionar fuera del array de objetos concesionaria
+/*buscarAuto = function(patente){ 
+   //console.log('patente' + patente);
+return autos.find(auto => auto.patente == patente) || null;
+}*/
 let concesionaria = {
     /* completar */
       autos: autos,
-      buscarAuto : function(patente){
-      return autos.find(auto => auto[patente] == patente) || null;
+      buscarAuto : function(patente){ 
+         //console.log('patente' + patente);
+      return autos.find(auto => auto.patente == patente) || null;
       },
-      /*venderAuto: function(patente){
-      for (let i = 0; i < autos.length; i++){
+      venderAuto: function(patente){
          let auto  = this.buscarAuto(patente);
          if (!auto){
             return 'El auto no existe, no está registrado'; 
@@ -16,7 +21,10 @@ let concesionaria = {
          }
          auto.vendido =  true;
          return 'El auto fue vendido correctamente';
-         }
-      }*/   
-}
-console.log(concesionaria.buscarAuto("A123"));
+      },
+      autosParaLaVenta: () => {
+         return autos.filter(auto => auto.vendido == false);
+      }      
+}   
+//console.log(buscarAuto());
+console.log(concesionaria.autosParaLaVenta());
