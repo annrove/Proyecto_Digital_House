53dea909a534518b909658a4e47ea1f3cbd587f0
annrove <annrove@gmail.com>
2022-03-22 09:49:45 -0500

    Nuevos Cambios a los archivos del Proyecto 9:49_22/03/22

--- Autos1.js
+++ Autos1.js
@@ -2,27 +2,21 @@ const autos = require('./autos')
 let concesionaria = {
     /* completar */
       autos: autos,
-      buscarAuto : function(patente, valor){
-      return autos.find(auto => auto[patente] == valor) || null;
-      },   
-      //buscarAutoPatente: function(patente){
-      //return this.buscarAuto('patente', patente);
-      //},
-      venderAuto: function(patente){
-      let auto  = this.buscarAuto(patente);
-      if (!auto){
-      return 'El auto no existe, no está registrado'; 
-      }
-      if (auto.vendido){
-      return 'No se puede verder un carro que ya está vendido';  
-      }
-      auto.vendido = true;
-      return 'El auto fue vendido correctamente';
-   },
-      autosParaLaVenta: function(vendido){
-      let lista = this.autos.filter(function(vendido){
-         return autos.vendido = false;
-      });  
-   }
+      buscarAuto : function(patente){
+      return autos.find(auto => auto[patente] == patente) || null;
+      },
+      /*venderAuto: function(patente){
+      for (let i = 0; i < autos.length; i++){
+         let auto  = this.buscarAuto(patente);
+         if (!auto){
+            return 'El auto no existe, no está registrado'; 
+         }
+         if (auto.vendido){
+            return 'No se puede verder un carro que ya está vendido';  
+         }
+         auto.vendido =  true;
+         return 'El auto fue vendido correctamente';
+         }
+      }*/   
 }
-console.log(autosParaLaVenta);
+console.log(concesionaria.buscarAuto("A123"));
